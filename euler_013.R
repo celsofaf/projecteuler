afebd4fb74e803f58c8419b497caf1add454875d
celsofaf <celsofaf@users.noreply.github.com>
2014-03-17 13:32:24 -0300

    Update euler_013.R

--- euler_013.R
+++ euler_013.R
@@ -1,6 +1,6 @@
 euler_013 <- function() {
   nums <- readLines("number_013.txt")
-  nums <- substr(nums, 1, 11)
-  nums <- as.character(sum(as.numeric(nums)))
+  nums <- as.numeric(substr(nums, 1, 11))
+  nums <- as.character(sum(nums))
   return(substr(nums, 1, 10))
 }
