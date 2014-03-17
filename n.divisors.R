4c36c1fee191f0c623ac6fb7aa855495a58b8fdd
celsofaf <celsofaf@users.noreply.github.com>
2014-03-17 10:20:09 -0300

    Update n.divisors.R

--- n.divisors.R
+++ n.divisors.R
@@ -1,11 +1,13 @@
 n.divisors <- function(n) {
   
   # returns the number of positive divisors of n >= 1
-  
-  if ( n == 1 ) return(1)
-  
+
   source("prime.factors.R")
-  factors <- prime.factors(n)
+  source("sieve.R")
+  if ( n == 0 ) return(0)
+  if ( n <= 3 ) return(1)
+  sieve <- sieve(floor(sqrt(n)))
+  factors <- prime.factors(n, sieve)
   ndiv <- 1
   for ( i in factors$exp )
     ndiv <- ndiv * (i+1)
