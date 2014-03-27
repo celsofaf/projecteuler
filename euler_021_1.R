f0182800531c2689e27d23509913bb78728a1b14
celsofaf <celsofaf@users.noreply.github.com>
2014-03-27 17:08:09 -0300

    Update euler_021_1.R

--- euler_021_1.R
+++ euler_021_1.R
@@ -1,10 +1,12 @@
 euler_021_1 <- function() {
   #brute force method
+
+  MAX_N <- 10000
   
   sum.divisors <- function(n) {
-    if ( n <= 3 ) return(1)
+    if ( n %in% c(1, 2, 3, 5, 7) ) return(1)
     sum <- 1
-    for ( i in 2:floor(sqrt(n)) ) {
+    for ( i in seq(2+n%%2, floor(sqrt(n)), by=1+n%%2) ) {
       if ( n %% i == 0 ) {
         sum <- sum + i
         sum <- sum + n / i
@@ -15,11 +17,10 @@ euler_021_1 <- function() {
     return(sum)
   }
   
-  max_n <- 10000
   sum <- 0
-  for ( i in 1:max_n ) {
+  for ( i in 1:MAX_N ) {
     di <- sum.divisors(i)
-    if ( di > 1 && di <= max_n && sum.divisors(di) == i && di > i )
+    if ( di > 1 && di <= MAX_N && sum.divisors(di) == i && di > i )
       sum <- sum + i + di
   }
   return(sum)
