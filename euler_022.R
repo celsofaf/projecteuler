2cf97626b507fb2e52757a2a4cd3f9b9e27fd834
celsofaf <celsofaf@users.noreply.github.com>
2014-04-01 10:42:50 -0300

    Update euler_022.R

--- euler_022.R
+++ euler_022.R
@@ -1,13 +1,15 @@
-library("stringr")
-names <- readLines("names_022.txt")
-names <- str_replace_all(names, "\"", "")
-names <- str_split(names, ",")
-names <- sort(names[[1]])
-scoresum <- 0
-for ( i in 1:length(names) ) {
-  score <- 0
-  for ( j in 1:str_length(names[i]) )
-    score <- score + which(LETTERS == str_sub(names[i], j, j))
-  scoresum <- scoresum + score * i
+euler_022 <- function(){
+  library("stringr")
+  names <- readLines("names_022.txt")
+  names <- str_replace_all(names, "\"", "")
+  names <- str_split(names, ",")
+  names <- sort(names[[1]])
+  scoresum <- 0
+  for ( i in 1:length(names) ) {
+    score <- 0
+    for ( j in 1:str_length(names[i]) )
+      score <- score + which(LETTERS == str_sub(names[i], j, j))
+    scoresum <- scoresum + score * i
+  }
+  return(scoresum)
 }
-print(scoresum)
